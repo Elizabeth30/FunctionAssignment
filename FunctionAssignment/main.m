7af037d3522d5f3ca066ba704ab29a5c25a04308
Elizabeth <ElizabethSanchez@crbc.net>
2015-04-09 09:48:01 -0400

    Initial Commit

---
+++ FunctionAssignment/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  FunctionAssignment
+//
+//  Created by Liz Sanchez on 4/9/15.
+//  Copyright (c) 2015 Liz Sanchez. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
